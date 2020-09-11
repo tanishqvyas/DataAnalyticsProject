8c2ef9750ace6c2810dcd26d37bd5dba105e1001
tanishqvyas <tanishqvyas069@gmail.com>
2020-09-11 20:42:47 +0530

    Added function to map string to int

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -4,7 +4,14 @@ import os
 class Preprocessor:
 
     def __init__(self):
-        pass    	
+        pass
+
+    # This function fetches all the values in a column and returns the list
+    def get_column_as_list(self, dataFrame, col_name):
+        
+        col_list = list(dataFrame[col_name].tolist())
+
+        return col_list	
 
 
     # Function to save modified dataset as a new version of DataFrame
@@ -18,9 +25,20 @@ class Preprocessor:
 
 
     # function to replace the Strings with respective numbers
-    def change_col_val_string_to_numeric():
-        pass  
+    def change_col_val_string_to_numeric(self, dataFrame, col_name, mapping_dict):
+        
+        # Fetching the current list
+        col_list = self.get_column_as_list(dataFrame, col_name)
 
+        changed_list = []
+
+        for i in col_list:
+
+            changed_list.append(mapping_dict[i])
+
+        dataFrame[col_name] = changed_list
+
+        return dataFrame
 
 
 
--- main.py
+++ main.py
@@ -19,11 +19,10 @@ from backend.preprocessing import Preprocessor
 meta_data = {
 
 	"wannaPreprocess" : True,
-	"wannaPlot" : True,
+	"wannaPlot" : False,
 }
 
 # Objects of Custom class imports
-preprocessorObj = Preprocessor()
 
 
 ### Global Paths
@@ -39,7 +38,7 @@ processed_csv_path = os.path.join(data_dir_path, "processeddata.csv")
 
 # Getting the dataframe from the initialdata.csv
 initialDataFrame = pd.DataFrame(pd.read_csv(initial_csv_path))
-print("Churn Dataset Sample View (Initial)\n")
+print("\nChurn Dataset Sample View (Initial)\n")
 print(initialDataFrame.head(), "\n")
 
 
@@ -48,7 +47,27 @@ print(initialDataFrame.head(), "\n")
 #------------------------------ STEPS -----------------------------------------#
 
 if(meta_data["wannaPreprocess"]):
-	pass
+	
+	# Making the object of the Preprocessor
+	preprocessorObj = Preprocessor()
+
+	# Mapping Dicts
+	Geography_mapping = {
+		"France": 0,
+		"Germany" : 1,
+		"Spain" : 2
+	}
+
+	Gender_mapping = {
+		"Female" : 0,
+		"Male" : 1
+	}
+
+	# Chaneg strings to integers
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Geography", Geography_mapping)
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Gender", Gender_mapping)
+	print("Processed Churn Dataset\n")
+	print(initialDataFrame.head(), "\n")
 
 
 
