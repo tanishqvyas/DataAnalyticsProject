f730ab894eda5925d1bb2a261d229957afb8d1a3
tanishqvyas <tanishqvyas069@gmail.com>
2020-09-17 10:57:55 +0530

    basic preprocessing

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ from backend.preprocessing import Preprocessor
 meta_data = {
 
 	"wannaPreprocess" : True,
-	"wannaPlot" : True,
-	"wannaTrain" : False
+	"wannaPlot" : False,
+	"wannaTrain" : False,
+	"wannaTest" : False
 }
 
 # Objects of Custom class imports
@@ -50,26 +51,56 @@ print(initialDataFrame.head(), "\n")
 # Preprocessing the data
 if(meta_data["wannaPreprocess"]):
 	
+	# Removing Duplicates
+	initialDataFrame = initialDataFrame[~initialDataFrame.duplicated()]
+
+	# Removing Data entries with
+
+	
 	# Making the object of the Preprocessor
 	preprocessorObj = Preprocessor()
 
-	# Mapping Dicts
-	Gender_mapping = {
+	# Mapping Dictionary
+	String_to_Num_mapping = {
 		"Female" : 0,
-		"Male" : 1
-	}
-
-	Yes_No_mapping = {
+		"Male" : 1,
+		
 		"Yes" : 1,
 		"No" : 0,
-		"No internet service": 2
+		"No internet service": 2,
+		"No phone service": 2,
+
+		"DSL" : 1,
+		"Fiber optic": 2,
+
+		"Month-to-month": 0,
+		"One year": 1,
+		"Two year": 2,
+
+		"Bank transfer (automatic)": 0,
+		"Credit card (automatic)": 1,
+		"Electronic check": 2,
+		"Mailed check": 3
 	}
 
 	# Chaneg strings to integers
-	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Partner", Yes_No_mapping)	
-	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Dependents", Yes_No_mapping)	
-	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "PhoneService", Yes_No_mapping)	
-	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "StreamingMovies", Yes_No_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Partner", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Dependents", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "PhoneService", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "StreamingMovies", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "PaymentMethod", String_to_Num_mapping)
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Contract", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "PaperlessBilling", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "StreamingTV", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "TechSupport", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "DeviceProtection", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "OnlineBackup", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "OnlineSecurity", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "InternetService", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "MultipleLines", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "gender", String_to_Num_mapping)	
+	initialDataFrame = preprocessorObj.change_col_val_string_to_numeric(initialDataFrame, "Churn", String_to_Num_mapping)	
+
 
 	
 	print("Processed Churn Dataset\n")
@@ -88,6 +119,8 @@ dataFrame = dataFrame[1:2000]
 
 # Plotting the data
 if(meta_data["wannaPlot"]):
+
+
 	
 	# Creating the object for Plotter class
 	plotterObj = Plotter(dataFrame)
@@ -102,4 +135,9 @@ if(meta_data["wannaPlot"]):
 
 # Training the model
 if(meta_data["wannaTrain"]):
+	pass
+
+
+# Testing the model
+if(meta_data["wannaTest"]):
 	pass
