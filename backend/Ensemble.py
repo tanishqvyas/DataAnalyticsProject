4d3965ed59f6360eb03fab297e1968c2f3ec9880
tanishqvyas <tanishqvyas069@gmail.com>
2020-11-20 18:06:43 +0530

    modified stacked ensemble model

--- backend/Ensemble.py
+++ backend/Ensemble.py
@@ -1,30 +1,15 @@
 # make a prediction with a stacking ensemble
 from sklearn.datasets import make_regression
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.svm import SVR
-from sklearn.ensemble import StackingRegressor
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.svm import SVR, SVC
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import StackingRegressor, StackingClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve, auc
 
-
-# define dataset
-X, y = make_regression(n_samples=1000, n_features=20, n_informative=15, noise=0.1, random_state=1)
-# define the base models
-level0 = list()
-level0.append(('knn', KNeighborsRegressor()))
-level0.append(('cart', DecisionTreeRegressor()))
-level0.append(('svm', SVR()))
-# define meta learner model
-level1 = LinearRegression()
-# define the stacking ensemble
-model = StackingRegressor(estimators=level0, final_estimator=level1, cv=5)
-# fit the model on all available data
-model.fit(X, y)
-# make a prediction for one example
-data = [[0.59332206,-0.56637507,1.34808718,-0.57054047,-0.72480487,1.05648449,0.77744852,0.07361796,0.88398267,2.02843157,1.01902732,0.11227799,0.94218853,0.26741783,0.91458143,-0.72759572,1.08842814,-0.61450942,-0.69387293,1.69169009]]
-yhat = model.predict(data)
-print('Predicted Value: %.3f' % (yhat))
 
 
 
@@ -32,13 +17,34 @@ def Stacked_Ensemble(x_train, x_test, y_train, y_test):
 
     # define the base models
     level0 = list()
+    level0.append(('lr', LinearRegression()))
     level0.append(('knn', KNeighborsRegressor()))
     level0.append(('cart', DecisionTreeRegressor()))
     level0.append(('svm', SVR()))
+    level0.append(('adaboost', AdaBoostRegressor()))
+    # level0.append(('bayes', ))
+    
+
+    # Classifier
+    # level0.append(('lr', LogisticRegression()))
+    # level0.append(('knn', KNeighborsClassifier()))
+    # level0.append(('cart', DecisionTreeClassifier()))
+    # level0.append(('svm', SVC()))
+    # level0.append(('bayes', GaussianNB()))
+
+
     # define meta learner model
     level1 = LinearRegression()
+
+    # Classifier
+    # level1 = LogisticRegression()
+
+
+
     # define the stacking ensemble
     model = StackingRegressor(estimators=level0, final_estimator=level1, cv=5)
+    # model = StackingClassifier(estimators=level0, final_estimator=level1, cv=5)
+
 
     model.fit(x_train, y_train)
 
